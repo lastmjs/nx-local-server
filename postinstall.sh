e242db6496b2ceb5ff945a7c98f4e712e4770178
Jordan Last <jordan.michael.last@gmail.com>
2017-02-27 13:49:01 -0700

    test

--- postinstall.sh
+++ postinstall.sh
@@ -4,7 +4,7 @@ wget http://nginx.org/download/nginx-1.10.1.tar.gz
 tar -xzvf nginx-1.10.1.tar.gz
 rm nginx-1.10.1.tar.gz
 cd nginx-1.10.1
-./configure --prefix=.. --error-log-path=node_modules/nx-local-server/logs/error.log
+./configure --prefix=.. --error-log-path=./logs/error.log
 make
 make install
 cd ..
